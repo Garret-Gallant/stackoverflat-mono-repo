5e8f37051dce2603e905488a7e2c4fe62a2b8b8e
Jared <mumawjared@gmail.com>
2022-09-12 14:55:29 -0700

    linter

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,11 @@
+# Categories for posts
+
 class CategoriesController < ApplicationController
+  def index
+    render json: Category.all
+  end
+
+  def show
+    render json: Category.find(params[:id])
+  end
 end
