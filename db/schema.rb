10de01fffb417ba1cf403b07fd1f2adbd7b9c37b
Jenipher Belloso <jenipherbelloso@gmail.com>
2022-09-13 11:11:30 -0600

    added stuff

--- db/schema.rb
+++ db/schema.rb
@@ -60,14 +60,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_12_222405) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
-    t.string "email"
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "is_admin"
     t.datetime "confirmed_at"
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "comment_likes", "comments"
