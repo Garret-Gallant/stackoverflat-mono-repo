550aab95183a921ebe351b53d303e15c58032070
Jared <mumawjared@gmail.com>
2022-09-15 13:36:25 -0700

    comment form component

--- forum-project-frontend/src/components/CommentList.js
+++ forum-project-frontend/src/components/CommentList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import CommentForm from "./CommentForm";
 import { useState, useEffect } from "react";
 
 const CommentList = ({ postId }) => {
@@ -12,18 +13,28 @@ const CommentList = ({ postId }) => {
   }, []);
 
   if (comments.length === 0) {
-    return <div className="comment-list">No comments yet.</div>;
+    return (
+      <div className="comment-list">
+        <div>No comments yet.</div>
+        <CommentForm postId={postId} />
+      </div>
+    );
   }
   return (
     <div>
-      {comments.map((comment) => {
-        return (
-          <div className="comment-list">
-            <span>{comment.user.username}</span>
-            <p>{comment.body}</p>
-          </div>
-        );
-      })}
+      <div>
+        {comments.map((comment) => {
+          return (
+            <div className="comment-list">
+              <span>{comment.user.username}</span>
+              <p>{comment.body}</p>
+            </div>
+          );
+        })}
+      </div>
+      <div className="comment-list">
+        <CommentForm postId={postId} />
+      </div>
     </div>
   );
 };
